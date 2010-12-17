824655c0bb9ef3b9daad61e51de9b8a2eb3b63e7
srikanth.allu <srikanth.allu@380e499b-fd06-67ba-bccf-d8f490d6f30f>
2010-12-17 21:14:54 +0000

    First commit,,,Unstructured Poisson Operator header.

---
+++ Uniform/UnstructuredPoissonOperator.h
@@ -0,0 +1,18 @@
+//----------------------------------------------- 
+// AFBM Unstructured Poisson - Rahul & Srikanth  
+//----------------------------------------------- 
+#ifndef included_UnstructuredPoissonoperator
+#define included_UnstructuredPoissonoperator
+
+#include "petsc.h"
+#include "PetscMatrix.h"
+#include "petscvec.h"
+
+createNeumann(unsigned int Dim);
+
+dirichletMatCorrection();
+
+dirichletVecCorrection();
+
+computeRHS();
+
