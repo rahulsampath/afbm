d784fd61ccf5ffc371b24f8225b9ed562b40c00a
rahul.sampath <rahul.sampath@380e499b-fd06-67ba-bccf-d8f490d6f30f>
2010-12-19 13:41:31 +0000

    starting fullDomain functions

--- Uniform/fullDomainUtils.C
+++ Uniform/fullDomainUtils.C
@@ -1,4 +1,17 @@
 
 #include "fullDomainUtils.h"
 
+void createNeumannMatrix_Full(DA da, Mat mat) {
+}
+
+void applyDirichletMatrixCorrection_Full(DA da, Mat mat) {
+}
+
+void setDirichletValues_Full(DA da, Vec vec) {
+}
+
+void computeRHS_Full(DA da, Vec rhs) {
+}
+
+
 
--- Uniform/fullDomainUtils.h
+++ Uniform/fullDomainUtils.h
@@ -2,6 +2,17 @@
 #ifndef __FULL_DOMAIN__ 
 #define __FULL_DOMAIN__
 
+#include "petscda.h"
+
+void createNeumannMatrix_Full(DA da, Mat mat);
+
+void applyDirichletMatrixCorrection_Full(DA da, Mat mat);
+
+void setDirichletValues_Full(DA da, Vec vec);
+
+void computeRHS_Full(DA da, Vec rhs);
 
 #endif
+
+
 
