c1452f00772955a05d50682bcb5f00d2ae69989f
rahul.sampath <rahul.sampath@380e499b-fd06-67ba-bccf-d8f490d6f30f>
2010-12-24 21:40:44 +0000

    dummy

--- Uniform/fullDomainUtils.C
+++ Uniform/fullDomainUtils.C
@@ -5,6 +5,10 @@
 
 #include "global.h"
 
+#define __RG_NODE_ID__(xi, yi, zi, N) ( ( ( ((zi)*(N)) + (yi) )*(N) ) + (xi) )
+
+extern double stencil[64];
+
 void createNeumannMatrix_Full(DA da, Mat mat) {
 
   PetscInt N;
@@ -18,7 +22,14 @@ void createNeumannMatrix_Full(DA da, Mat mat) {
     for(int yi = 0; yi < (N - 1); yi++) {
       for(int xi = 0; xi < (N - 1); xi++) {
         PetscInt indices[8];
-
+        indices[0] = __RG_NODE_ID__(xi, yi, zi, N);
+        indices[1] = __RG_NODE_ID__((xi + 1), yi, zi, N);
+        indices[2] = __RG_NODE_ID__(xi, (yi + 1), zi, N);
+        indices[3] = __RG_NODE_ID__((xi + 1), (yi + 1), zi, N);
+        indices[4] = __RG_NODE_ID__(xi, yi, (zi + 1), N);
+        indices[5] = __RG_NODE_ID__((xi + 1), yi, (zi + 1), N);
+        indices[6] = __RG_NODE_ID__(xi, (yi + 1), (zi + 1), N);
+        indices[7] = __RG_NODE_ID__((xi + 1), (yi + 1), (zi + 1), N);
         MatSetValues(mat, 8, indices, 8, indices, stencil, ADD_VALUES);
       }//end for xi
     }//end for yi
@@ -26,6 +37,8 @@ void createNeumannMatrix_Full(DA da, Mat mat) {
 
   MatAssemblyBegin(mat, MAT_FINAL_ASSEMBLY);
   MatAssemblyEnd(mat, MAT_FINAL_ASSEMBLY);
+
+  MatScale(mat, (h/2.0));
 
 }
 
