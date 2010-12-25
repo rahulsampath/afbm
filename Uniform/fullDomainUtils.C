3d602414f1adaf74f5cc59071e09cd0ed44b77c5
rahul.sampath <rahul.sampath@380e499b-fd06-67ba-bccf-d8f490d6f30f>
2010-12-25 02:44:48 +0000

    added dirichlet matrix correction for full domain

--- Uniform/fullDomainUtils.C
+++ Uniform/fullDomainUtils.C
@@ -43,12 +43,198 @@ void createNeumannMatrix_Full(DA da, Mat mat) {
 }
 
 void applyDirichletMatrixCorrection_Full(DA da, Mat mat) {
+
+  PetscInt N;
+  DAGetInfo(da, PETSC_NULL, &N, PETSC_NULL, PETSC_NULL, 
+      PETSC_NULL, PETSC_NULL, PETSC_NULL, PETSC_NULL, PETSC_NULL, PETSC_NULL, PETSC_NULL);
+
+  {
+    //Left
+    int xi = 0;
+    for(int zi = 0; zi < N; zi++) {
+      for(int yi = 0; yi < N; yi++) {
+        int curr = __RG_NODE_ID__(xi, yi, zi, N);
+        MatSetValue(mat, curr, curr, 1.0, INSERT_VALUES);
+        for(int zj = (zi - 1); zj <= (zi + 1); zj++) {
+          if((zj >= 0) && (zj < N)) {
+            for(int yj = (yi - 1); yj <= (yi + 1); yj++) {
+              if((yj >= 0) && (yj < N)) {
+                for(int xj = (xi - 1); xj <= (xi + 1); xj++) {
+                  if((xj >= 0) && (xj < N)) {
+                    int other = __RG_NODE_ID__(xj, yj, zj, N);
+                    if(curr != other) {
+                      MatSetValue(mat, curr, other, 0.0, INSERT_VALUES);
+                      MatSetValue(mat, other, curr, 0.0, INSERT_VALUES);
+                    }
+                  }
+                }//end for xj
+              }
+            }//end for yj
+          }
+        }//end for zj
+      }//end for yi
+    }//end for zi
+  }
+
+  {
+    //Right
+    int xi = N - 1;
+    for(int zi = 0; zi < N; zi++) {
+      for(int yi = 0; yi < N; yi++) {
+        int curr = __RG_NODE_ID__(xi, yi, zi, N);
+        MatSetValue(mat, curr, curr, 1.0, INSERT_VALUES);
+        for(int zj = (zi - 1); zj <= (zi + 1); zj++) {
+          if((zj >= 0) && (zj < N)) {
+            for(int yj = (yi - 1); yj <= (yi + 1); yj++) {
+              if((yj >= 0) && (yj < N)) {
+                for(int xj = (xi - 1); xj <= (xi + 1); xj++) {
+                  if((xj >= 0) && (xj < N)) {
+                    int other = __RG_NODE_ID__(xj, yj, zj, N);
+                    if(curr != other) {
+                      MatSetValue(mat, curr, other, 0.0, INSERT_VALUES);
+                      MatSetValue(mat, other, curr, 0.0, INSERT_VALUES);
+                    }
+                  }
+                }//end for xj
+              }
+            }//end for yj
+          }
+        }//end for zj
+      }//end for yi
+    }//end for zi
+  }
+
+  {
+    //Front
+    int yi = 0;
+    for(int zi = 0; zi < N; zi++) {
+      for(int xi = 0; xi < N; xi++) {
+        int curr = __RG_NODE_ID__(xi, yi, zi, N);
+        MatSetValue(mat, curr, curr, 1.0, INSERT_VALUES);
+        for(int zj = (zi - 1); zj <= (zi + 1); zj++) {
+          if((zj >= 0) && (zj < N)) {
+            for(int yj = (yi - 1); yj <= (yi + 1); yj++) {
+              if((yj >= 0) && (yj < N)) {
+                for(int xj = (xi - 1); xj <= (xi + 1); xj++) {
+                  if((xj >= 0) && (xj < N)) {
+                    int other = __RG_NODE_ID__(xj, yj, zj, N);
+                    if(curr != other) {
+                      MatSetValue(mat, curr, other, 0.0, INSERT_VALUES);
+                      MatSetValue(mat, other, curr, 0.0, INSERT_VALUES);
+                    }
+                  }
+                }//end for xj
+              }
+            }//end for yj
+          }
+        }//end for zj
+      }//end for xi
+    }//end for zi
+  }
+
+  {
+    //Back
+    int yi = N - 1;
+    for(int zi = 0; zi < N; zi++) {
+      for(int xi = 0; xi < N; xi++) {
+        int curr = __RG_NODE_ID__(xi, yi, zi, N);
+        MatSetValue(mat, curr, curr, 1.0, INSERT_VALUES);
+        for(int zj = (zi - 1); zj <= (zi + 1); zj++) {
+          if((zj >= 0) && (zj < N)) {
+            for(int yj = (yi - 1); yj <= (yi + 1); yj++) {
+              if((yj >= 0) && (yj < N)) {
+                for(int xj = (xi - 1); xj <= (xi + 1); xj++) {
+                  if((xj >= 0) && (xj < N)) {
+                    int other = __RG_NODE_ID__(xj, yj, zj, N);
+                    if(curr != other) {
+                      MatSetValue(mat, curr, other, 0.0, INSERT_VALUES);
+                      MatSetValue(mat, other, curr, 0.0, INSERT_VALUES);
+                    }
+                  }
+                }//end for xj
+              }
+            }//end for yj
+          }
+        }//end for zj
+      }//end for xi
+    }//end for zi
+  }
+
+  {
+    //Bottom
+    int zi = 0;
+    for(int yi = 0; yi < N; yi++) {
+      for(int xi = 0; xi < N; xi++) {
+        int curr = __RG_NODE_ID__(xi, yi, zi, N);
+        MatSetValue(mat, curr, curr, 1.0, INSERT_VALUES);
+        for(int zj = (zi - 1); zj <= (zi + 1); zj++) {
+          if((zj >= 0) && (zj < N)) {
+            for(int yj = (yi - 1); yj <= (yi + 1); yj++) {
+              if((yj >= 0) && (yj < N)) {
+                for(int xj = (xi - 1); xj <= (xi + 1); xj++) {
+                  if((xj >= 0) && (xj < N)) {
+                    int other = __RG_NODE_ID__(xj, yj, zj, N);
+                    if(curr != other) {
+                      MatSetValue(mat, curr, other, 0.0, INSERT_VALUES);
+                      MatSetValue(mat, other, curr, 0.0, INSERT_VALUES);
+                    }
+                  }
+                }//end for xj
+              }
+            }//end for yj
+          }
+        }//end for zj
+      }//end for xi
+    }//end for yi
+  }
+
+  {
+    //Top
+    int zi = N - 1;
+    for(int yi = 0; yi < N; yi++) {
+      for(int xi = 0; xi < N; xi++) {
+        int curr = __RG_NODE_ID__(xi, yi, zi, N);
+        MatSetValue(mat, curr, curr, 1.0, INSERT_VALUES);
+        for(int zj = (zi - 1); zj <= (zi + 1); zj++) {
+          if((zj >= 0) && (zj < N)) {
+            for(int yj = (yi - 1); yj <= (yi + 1); yj++) {
+              if((yj >= 0) && (yj < N)) {
+                for(int xj = (xi - 1); xj <= (xi + 1); xj++) {
+                  if((xj >= 0) && (xj < N)) {
+                    int other = __RG_NODE_ID__(xj, yj, zj, N);
+                    if(curr != other) {
+                      MatSetValue(mat, curr, other, 0.0, INSERT_VALUES);
+                      MatSetValue(mat, other, curr, 0.0, INSERT_VALUES);
+                    }
+                  }
+                }//end for xj
+              }
+            }//end for yj
+          }
+        }//end for zj
+      }//end for xi
+    }//end for yi
+  }
+
+  MatAssemblyBegin(mat, MAT_FINAL_ASSEMBLY);
+  MatAssemblyEnd(mat, MAT_FINAL_ASSEMBLY);
+
 }
 
 void setDirichletValues_Full(DA da, Vec vec) {
+
+  PetscInt N;
+  DAGetInfo(da, PETSC_NULL, &N, PETSC_NULL, PETSC_NULL, 
+      PETSC_NULL, PETSC_NULL, PETSC_NULL, PETSC_NULL, PETSC_NULL, PETSC_NULL, PETSC_NULL);
+
 }
 
 void computeRHS_Full(DA da, Vec rhs) {
+
+  PetscInt N;
+  DAGetInfo(da, PETSC_NULL, &N, PETSC_NULL, PETSC_NULL, 
+      PETSC_NULL, PETSC_NULL, PETSC_NULL, PETSC_NULL, PETSC_NULL, PETSC_NULL, PETSC_NULL);
+
 }
 
 double phi(int node, double psi, double eta, double gamma) {
