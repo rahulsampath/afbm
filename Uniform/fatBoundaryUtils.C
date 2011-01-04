9beb2e8b3f80c066905d9e770ea81e799d74d8a2
rahul.sampath <rahul.sampath@380e499b-fd06-67ba-bccf-d8f490d6f30f>
2011-01-04 21:41:41 +0000

    minor correction

--- Uniform/fatBoundaryUtils.C
+++ Uniform/fatBoundaryUtils.C
@@ -34,92 +34,92 @@
 void createNeumannMatrix_Fat(Mat & petscMat)
 {
 
-      Mesh fatBoundary (3);
+  Mesh fatBoundary (3);
 
-      fatBoundary.read("hollowsphere.e");
+  fatBoundary.read("hollowsphere.e");
 
-      EquationSystems equation_systems (fatBoundary);
+  EquationSystems equation_systems (fatBoundary);
 
-      LinearImplicitSystem & system = 
-        equation_systems.add_system<LinearImplicitSystem> ("Poisson");
+  LinearImplicitSystem & system = 
+    equation_systems.add_system<LinearImplicitSystem> ("Poisson");
 
-      system.add_variable ("V", FIRST);
-      equation_systems.init ();
+  system.add_variable ("V", FIRST);
+  equation_systems.init ();
 
-      const MeshBase & mesh = equation_systems.get_mesh();
+  const MeshBase & mesh = equation_systems.get_mesh();
 
-      const unsigned int V_var = system.variable_number ("V");
+  const unsigned int V_var = system.variable_number ("V");
 
-      FEType fe_temp_type = system.variable_type(V_var);
+  FEType fe_temp_type = system.variable_type(V_var);
 
-      AutoPtr<FEBase> fe_temp  (FEBase::build(3, fe_temp_type));
+  AutoPtr<FEBase> fe_temp  (FEBase::build(3, fe_temp_type));
 
-      QGauss qrule (3, fe_temp_type.default_quadrature_order());
+  QGauss qrule (3, fe_temp_type.default_quadrature_order());
 
-      fe_temp->attach_quadrature_rule (&qrule);
+  fe_temp->attach_quadrature_rule (&qrule);
 
-      const std::vector<Real>& JxW = fe_temp->get_JxW();
+  const std::vector<Real>& JxW = fe_temp->get_JxW();
 
-      const std::vector<Point>& q_point = fe_temp->get_xyz();
+  const std::vector<Point>& q_point = fe_temp->get_xyz();
 
-      const std::vector<std::vector<RealGradient> >& dphi = fe_temp->get_dphi();
+  const std::vector<std::vector<RealGradient> >& dphi = fe_temp->get_dphi();
 
-      const std::vector<std::vector<Real> >& phi = fe_temp->get_phi();
+  const std::vector<std::vector<Real> >& phi = fe_temp->get_phi();
 
-      const DofMap & dof_map = system.get_dof_map();
+  const DofMap & dof_map = system.get_dof_map();
 
-      DenseMatrix<Number> Ke;
-      DenseVector<Number> Fe;
+  DenseMatrix<Number> Ke;
+  DenseVector<Number> Fe;
 
-      std::vector<unsigned int> dof_indices;
+  std::vector<unsigned int> dof_indices;
 
-      MeshBase::const_element_iterator       el     = mesh.active_local_elements_begin();
-      const MeshBase::const_element_iterator end_el = mesh.active_local_elements_end(); 
+  MeshBase::const_element_iterator       el     = mesh.active_local_elements_begin();
+  const MeshBase::const_element_iterator end_el = mesh.active_local_elements_end(); 
 
-      for ( ; el != end_el; ++el)
-      {    
-        const Elem* elem = *el;
+  for ( ; el != end_el; ++el)
+  {    
+    const Elem* elem = *el;
 
-        dof_map.dof_indices (elem, dof_indices);
+    dof_map.dof_indices (elem, dof_indices);
 
-        const unsigned int n_dofs   = dof_indices.size();
+    const unsigned int n_dofs   = dof_indices.size();
 
-        fe_temp->reinit  (elem);
+    fe_temp->reinit  (elem);
 
-        Ke.resize (n_dofs, n_dofs);
-        Fe.resize (n_dofs);
+    Ke.resize (n_dofs, n_dofs);
+    Fe.resize (n_dofs);
 
-        for (unsigned int qp = 0; qp < qrule.n_points(); qp++)
-        {
+    for (unsigned int qp = 0; qp < qrule.n_points(); qp++)
+    {
 
-          const Real x = __CENTER_X__ + q_point[qp](0);
-          const Real y = __CENTER_Y__ + q_point[qp](1);
-          const Real z = __CENTER_Z__ + q_point[qp](2);
+      const Real x = __CENTER_X__ + q_point[qp](0);
+      const Real y = __CENTER_Y__ + q_point[qp](1);
+      const Real z = __CENTER_Z__ + q_point[qp](2);
 
-          // Assemble the Poisson Operator equation 
-          for (unsigned int i = 0; i < n_dofs; i++) {
-            for (unsigned int j = 0; j < n_dofs; j++) {
-              Ke(i,j) += (JxW[qp]*dphi[i][qp]*dphi[j][qp]);
-            }
-          }
+      // Assemble the Poisson Operator equation 
+      for (unsigned int i = 0; i < n_dofs; i++) {
+        for (unsigned int j = 0; j < n_dofs; j++) {
+          Ke(i,j) += (JxW[qp]*dphi[i][qp]*dphi[j][qp]);
+        }
+      }
 
-          // Build the element right hand side
-          for (unsigned int i = 0; i < phi.size(); i++) {
-            Fe(i) += (__FN__(x, y, z)*JxW[qp]*phi[i][qp]);
-          }
+      // Build the element right hand side
+      for (unsigned int i = 0; i < phi.size(); i++) {
+        Fe(i) += (__FN__(x, y, z)*JxW[qp]*phi[i][qp]);
+      }
 
-        } // end of the quadrature point qp-loop
+    } // end of the quadrature point qp-loop
 
-        system.matrix->add_matrix (Ke, dof_indices);  //                 
-        system.rhs->add_vector    (Fe, dof_indices);  // 
-      } // end of element loop
+    system.matrix->add_matrix (Ke, dof_indices);  //                 
+    system.rhs->add_vector    (Fe, dof_indices);  // 
+  } // end of element loop
 
-      system.matrix->close();
+  system.matrix->close();
 
-      Mat OriginalMat = (dynamic_cast <PetscMatrix<Number>* > (system.matrix))->mat();
-      PetscErrorCode  ierr;
-      ierr = MatDuplicate( OriginalMat , MAT_COPY_VALUES, &petscMat);
-//      petscVec = (system.rhs)->vec();
+  Mat OriginalMat = (dynamic_cast <PetscMatrix<Number>* > (system.matrix))->mat();
+  PetscErrorCode  ierr;
+  ierr = MatDuplicate( OriginalMat , MAT_COPY_VALUES, &petscMat);
+  //      petscVec = (system.rhs)->vec();
 
 }
 
@@ -149,11 +149,11 @@ void dirichletMatCorrection_Fat(LinearImplicitSystem& system , Mat & stiffnesMat
             if( elem->node(n) == side->node(ns) ) { 
               for(unsigned int m = 0; m < elem->n_nodes(); m++) {
                 if(m!=n){
-                    MatSetValue ( stiffnesMatrix, m , n, 0.0, INSERT_VALUES);
-                    MatSetValue ( stiffnesMatrix, n , m, 0.0, INSERT_VALUES);
+                  MatSetValue ( stiffnesMatrix, m , n, 0.0, INSERT_VALUES);
+                  MatSetValue ( stiffnesMatrix, n , m, 0.0, INSERT_VALUES);
                 }
               }//end for m
-                MatSetValue ( stiffnesMatrix, n , n, 1.0, INSERT_VALUES);
+              MatSetValue ( stiffnesMatrix, n , n, 1.0, INSERT_VALUES);
             }
 
           }//end for n
@@ -196,7 +196,7 @@ void dirichletVecCorrection_Fat(LinearImplicitSystem& system , Vec dirichletVec,
             VecGetValues( dirichletVec, 1 , &idx , &dirichletValue );
             if( elem->node(n) == side->node(ns) ) { 
               system.rhs->set(dof_indices[n], dirichletValue );
-     //       MatSetValue ( & stiffnesMatrix, n , n, 1.0);
+              //       MatSetValue ( & stiffnesMatrix, n , n, 1.0);
             }
 
           }//end for n
@@ -273,7 +273,7 @@ void getDiracFunctions_Fat(LinearImplicitSystem& system, Vec & solVec, Vec & rhs
     const unsigned int n_dofs   = dof_indices.size();
 
     for(unsigned int s = 0; s < elem->n_sides(); s++) {
-      
+
       fe_face->reinit(elem, s);
 
       const short int bnd_id = (mesh.boundary_info)->boundary_id (elem, s);
@@ -281,13 +281,10 @@ void getDiracFunctions_Fat(LinearImplicitSystem& system, Vec & solVec, Vec & rhs
 
         for(unsigned int qp = 0; qp < qrule.n_points(); qp++) 
         {
-          double px = q_point[qp](0);
-          double py = q_point[qp](1);
-          double pz = q_point[qp](2);
+          double px = __CENTER_X__ + q_point[qp](0);
+          double py = __CENTER_Y__ + q_point[qp](1);
+          double pz = __CENTER_Z__ + q_point[qp](2);
 
-          px = px + __CENTER_X__;
-          py = py + __CENTER_Y__;
-          pz = pz + __CENTER_Z__;
           phi_Full(px, py, pz, N, indices, vals);
 
           for (unsigned int j=0; j<indices.size(); j++)
@@ -299,14 +296,12 @@ void getDiracFunctions_Fat(LinearImplicitSystem& system, Vec & solVec, Vec & rhs
 
             for (unsigned int l=0; l<dphi.size(); l++)
             {
-
               idx = dof_indices[l];
               VecGetValues( solVec, 1 , &idx , &solValue );
               dVdx +=  dphi[l][qp](0)*solValue;
               dVdy +=  dphi[l][qp](1)*solValue;
               dVdz +=  dphi[l][qp](2)*solValue;
-
-            }
+            }//nodes
 
             dVdn  = (dVdx*face_normal[qp](0) + dVdy*face_normal[qp](1) + dVdz*face_normal[qp](2) )*vals[j]*JxW[qp];
             VecSetValue(rhs, indices[j], dVdn , ADD_VALUES);
@@ -317,4 +312,10 @@ void getDiracFunctions_Fat(LinearImplicitSystem& system, Vec & solVec, Vec & rhs
 
     }//elem side iterator
   }// elem iterator
+
+  VecAssemblyBegin(rhs);
+  VecAssemblyEnd(rhs);
+
 }
+
+
