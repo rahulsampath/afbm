4686ee735c58f2b929247101db00c5fb3646d03f
rahul.sampath <rahul.sampath@380e499b-fd06-67ba-bccf-d8f490d6f30f>
2011-01-08 00:35:13 +0000

    dummy

--- trunk/Uniform/picardDriver.C
+++ trunk/Uniform/picardDriver.C
@@ -14,6 +14,7 @@ int main(int argc, char** argv) {
   PetscInitialize(&argc, &argv, "options", NULL);
   LibMeshInit init(argc, argv);
 
+  //Init stuff for Fat Boundary (Read mesh, build equation systems, dof-map)  
   Mesh fatBoundary (3);
 
   fatBoundary.read("hollowsphere.e");
@@ -27,6 +28,45 @@ int main(int argc, char** argv) {
   DofMap & dof_map = system.get_dof_map();
 
   MeshBase & mesh = equation_systems.get_mesh();
+
+  //Init stuff for Full domain (Create DA/DMMG) 
+  PetscInt N;
+  PetscOptionsGetInt(0, "-N", &N, 0);
+
+  //Create Full Domain solution vector (Initial Guess)
+  Vec solVecFull;
+  Vec rhsVecFull;
+
+  //Compute base RHS vec for full domain
+
+  //Call DMMGSetKSP and pass CreateMat. Set RHS function = NULL 
+
+  //Create Neumann Mat, base RHS vec for fat boundary
+
+  //Duplicate Neumann Mat and base RHS vec for fat boundary
+
+  //Create Fat boundary solution vector
+
+  //Create KSP for fat boundary 
+
+  //Picard block
+  {
+
+    //Call DirichletAddCorrection2 for fat boundary and add it to base RHS
+
+    //Call DirichletMatrixCorrection for bnd=1 and bnd=2 
+
+    //Call DirichletSetCorrection1 and DirichletSetCorrection2 for fat boundary
+
+    //Solve Fat boundary problem 
+
+    //Add dirac delta corrections to base RHS for full domain
+
+    //Call DirichletSetCorrection for RHS of full domain
+
+    //Solve Full domain
+
+  }//end for Picar block
 
   PetscFinalize();
 
