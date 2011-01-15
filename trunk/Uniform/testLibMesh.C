94b0f2682b62d7bd7dae35aa00ee71665b4b00b5
rahul.sampath <rahul.sampath@380e499b-fd06-67ba-bccf-d8f490d6f30f>
2011-01-15 16:09:47 +0000

    dummy

--- trunk/Uniform/testLibMesh.C
+++ trunk/Uniform/testLibMesh.C
@@ -1,28 +1,17 @@
 
 #include "petsc.h"
-#include "mesh.h"
-#include "equation_systems.h"
-#include "linear_implicit_system.h"
+#include "libmesh.h"
 
 int main(int argc, char** argv) {
 
   PetscInitialize(&argc, &argv, NULL, NULL);
-  LibMeshInit init(argc, argv, PETSC_COMM_SELF);
 
-  //Init stuff for Fat Boundary (Read mesh, build equation systems, dof-map)  
-  Mesh fatBoundary (3);
+  int rank;
+  MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
 
-  fatBoundary.read("hollowsphere.e");
-
-  EquationSystems equation_systems (fatBoundary);
-
-  LinearImplicitSystem & system =  equation_systems.add_system<LinearImplicitSystem> ("Poisson");
-  system.add_variable ("V", FIRST);
-  equation_systems.init ();
-
-  DofMap & dof_map = system.get_dof_map();
-
-  MeshBase & mesh = equation_systems.get_mesh();
+  if(rank == 0) {
+    LibMeshInit init(argc, argv, PETSC_COMM_SELF);
+  }
 
   PetscFinalize();
 
